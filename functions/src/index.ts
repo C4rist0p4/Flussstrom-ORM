84e5b0f03b40b956ad7e87ce85511af83e622176
C4rist0p4 <Christoph_S@posteo.de>
2020-03-23 18:39:05 +0100

    23.03

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,18 @@
 import * as functions from "firebase-functions";
 import { connect } from "./config";
 import { Meldungen } from "./entity/Meldungen";
+const auth = require("./validateFirebaseIdToken");
 
 export const getMeldung = functions.https.onRequest(async (req, res) => {
-  const connection = await connect();
-  const meldungenRepo = connection.getRepository(Meldungen);
+  const result = await auth(req, res);
+  if (result == false) {
+    res.status(403).send("Unauthorized");
+  } else {
+    const connection = await connect();
+    const meldungenRepo = connection.getRepository(Meldungen);
+    // all rows
+    const allMeldungen = await meldungenRepo.find();
 
-  // Get all rows
-  const allMeldungen = await meldungenRepo.find();
-
-  res.status(200).send({ data: { allMeldungen } });
+    res.status(200).send({ data: { allMeldungen } });
+  }
 });
---
+++ functions/src/validateFirebaseIdToken.ts
@@ -0,0 +1,44 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+module.exports = async (req, res) => {
+  if (
+    (!req.headers.authorization ||
+      !req.headers.authorization.startsWith("Bearer ")) &&
+    !(req.cookies && req.cookies.__session)
+  ) {
+    console.error(
+      "No Firebase ID token was passed as a Bearer token in the Authorization header.",
+      "Make sure you authorize your request by providing the following HTTP header:",
+      "Authorization: Bearer <Firebase ID Token>",
+      'or by passing a "__session" cookie.'
+    );
+    return false;
+  }
+
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    console.log('Found "Authorization" header');
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else if (req.cookies) {
+    console.log('Found "__session" cookie');
+    // Read the ID Token from cookie.
+    idToken = req.cookies.__session;
+  } else {
+    // No cookie
+    return false;
+  }
+
+  try {
+    await admin.auth().verifyIdToken(idToken);
+    return true;
+  } catch (error) {
+    console.error("Error while verifying Firebase ID token:", error);
+    return false;
+  }
+};
