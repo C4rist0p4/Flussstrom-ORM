423d78af4376863436fe485bfacea04c52978ba4
C4rist0p4 <Christoph_S@posteo.de>
2020-04-14 18:20:46 +0200

    getMeldungen revised

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,6 @@ import * as functions from "firebase-functions";
 import { connect } from "./config";
 import { Meldungen } from "./entity/Meldungen";
 import { Benutzer } from "./entity/Benutzer";
-import { Between } from "typeorm";
 const auth = require("./validateFirebaseIdToken");
 
 export const getMeldung = functions.https.onRequest(async (req, res) => {
@@ -10,12 +9,16 @@ export const getMeldung = functions.https.onRequest(async (req, res) => {
   if (result == false) {
     res.status(403).send("Unauthorized");
   } else {
+    const { fk_anlagen } = req.body.data;
     const connection = await connect();
     const meldungenRepo = connection.getRepository(Meldungen);
-    // all rows
-    const allMeldungen = await meldungenRepo.find({
-      datum: Between(new Date("2018-04-05"), new Date("2018-04-07")),
-    });
+
+    const allMeldungen = await meldungenRepo
+      .createQueryBuilder("meldungen")
+      .where("meldungen.fk_anlagen = :id", { id: fk_anlagen })
+      .orderBy("meldungen.datum", "DESC")
+      .take(3)
+      .getMany();
 
     res.status(200).json({ data: allMeldungen });
   }
